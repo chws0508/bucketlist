d3d71133798c76d0332e2664fca51278542743c0
chws0508 <chws0508@naver.com>
2024-04-28 23:20:20 +0900

    feat : 데모영상 추가

--- feature/updatecompletedbucket/src/main/java/com/woosuk/updatecompletedbucket/edit/EditCompletedBucketScreen.kt
+++ feature/updatecompletedbucket/src/main/java/com/woosuk/updatecompletedbucket/edit/EditCompletedBucketScreen.kt
@@ -3,49 +3,31 @@ package com.woosuk.updatecompletedbucket.edit
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerDefaults
-import androidx.compose.material3.DatePickerDialog
-import androidx.compose.material3.DatePickerState
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.woosuk.common.LocalDateTimeUtil.convertMillsToDateTime
 import com.woosuk.domain.model.CompletedBucket
 import com.woosuk.theme.WoosukTheme
-import com.woosuk.theme.defaultFontFamily
 import com.woosuk.updatecompletedbucket.R
+import com.woosuk.updatecompletedbucket.add.BucketCompleteDate
 import com.woosuk.updatecompletedbucket.add.BucketDiary
 import com.woosuk.updatecompletedbucket.add.BucketInfo
 import com.woosuk.updatecompletedbucket.add.BucketPhotos
 import ui.ArrowBackTopAppBar
 import ui.DefaultButton
-import ui.SingleLineTextField
-import ui.noRippleClickable
 import java.time.LocalDateTime
 
 @Composable
@@ -55,6 +37,7 @@ fun EditCompletedBucketRoute(
     onShowSnackBar: (String) -> Unit,
     onNavigateToCompletedBucketDetail: (bucketId: Int) -> Unit,
 ) {
+    val context = LocalContext.current
     val uiState = viewModel.uiState.collectAsStateWithLifecycle().value
     EditCompletedBucketScreen(
         onBackClick = onBackClick,
@@ -63,14 +46,15 @@ fun EditCompletedBucketRoute(
         onAddImage = viewModel::addImageUris,
         onImageDeleted = viewModel::deleteImage,
         updateCompletedBucket = viewModel::updateCompletedBucket,
-        onShowSnackBar = onShowSnackBar,
         onCompletedDateChanged = viewModel::onCompletedDateChanged,
     )
     LaunchedEffect(null) {
         viewModel.uiEvent.collect { uiEvent ->
             when (uiEvent) {
-                is EditCompletedBucketUiEvent.EditSuccess,
-                -> onNavigateToCompletedBucketDetail(uiEvent.bucketId)
+                is EditCompletedBucketUiEvent.EditSuccess -> {
+                    onNavigateToCompletedBucketDetail(uiEvent.bucketId)
+                    onShowSnackBar(context.getString(R.string.succeed_edit_record))
+                }
             }
         }
     }
@@ -85,7 +69,6 @@ fun EditCompletedBucketScreen(
     updateCompletedBucket: () -> Unit = {},
     onImageDeleted: (Int) -> Unit = {},
     onCompletedDateChanged: (LocalDateTime) -> Unit = {},
-    onShowSnackBar: (String) -> Unit = {},
 ) {
     val scrollState = rememberScrollState()
     Column(
@@ -134,84 +117,8 @@ fun EditCompletedBucketScreen(
                     .fillMaxWidth()
                     .padding(16.dp),
                 onEditCompletedBucket = updateCompletedBucket,
-                onShowSnackBar = onShowSnackBar,
             )
         }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun BucketCompleteDate(
-    modifier: Modifier,
-    completedDate: LocalDateTime,
-    onCompletedDateChanged: (LocalDateTime) -> Unit,
-) {
-    var showDatePicker by remember { mutableStateOf(false) }
-    val datePickerState = rememberDatePickerState()
-    Column(modifier = modifier) {
-        Row {
-            Text(
-                modifier = Modifier.padding(start = 5.dp),
-                text = stringResource(id = R.string.completed_date),
-                fontFamily = defaultFontFamily,
-                fontWeight = FontWeight.Normal,
-                fontSize = 12.sp,
-                color = WoosukTheme.colors.systemBlack,
-            )
-            Text(text = "*", fontFamily = defaultFontFamily, color = Color.Red)
-        }
-        SingleLineTextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .noRippleClickable { showDatePicker = true },
-            text = completedDate.toLocalDate().toString(),
-            hint = "",
-            onValueChange = {},
-            enabled = false,
-        )
-    }
-    if (showDatePicker) {
-        BucketDatePicker(
-            onDismissRequest = { showDatePicker = false },
-            onDateSelected = {
-                onCompletedDateChanged(it)
-                showDatePicker = false
-            },
-            datePickerState = datePickerState,
-        )
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun BucketDatePicker(
-    onDismissRequest: () -> Unit,
-    onDateSelected: (LocalDateTime) -> Unit,
-    datePickerState: DatePickerState,
-) {
-    DatePickerDialog(
-        colors = DatePickerDefaults.colors(
-            containerColor = WoosukTheme.colors.systemWhite,
-        ),
-        onDismissRequest = onDismissRequest,
-        confirmButton = {
-            Button(
-                onClick = {
-                    val selectedDateMills = datePickerState.selectedDateMillis ?: return@Button
-                    onDateSelected(selectedDateMills.convertMillsToDateTime())
-                },
-            ) {
-                Text(text = "선택")
-            }
-        },
-    ) {
-        DatePicker(
-            state = datePickerState,
-            colors = DatePickerDefaults.colors(
-                containerColor = WoosukTheme.colors.systemWhite,
-            ),
-        )
     }
 }
 
@@ -219,14 +126,10 @@ fun BucketDatePicker(
 fun EditButton(
     modifier: Modifier = Modifier,
     onEditCompletedBucket: () -> Unit,
-    onShowSnackBar: (String) -> Unit,
 ) {
     DefaultButton(
         modifier = modifier,
-        onClick = {
-            onEditCompletedBucket()
-            onShowSnackBar("달성 카드가 수정되었어요!")
-        },
+        onClick = { onEditCompletedBucket() },
         text = stringResource(R.string.edit_completed_bucket_button_text),
         enabled = true,
     )
@@ -239,7 +142,6 @@ fun EditCompletedBucketScreenPreview() {
         EditCompletedBucketScreen(
             onBackClick = {},
             uiState = CompletedBucket.mock(1),
-            onShowSnackBar = {},
         )
     }
 }
